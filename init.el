5ef607f5828a9c6dfdf80e951d38e18e68532b3d
Akash Manohar J <akash@akash.im>
2011-11-16 09:33:07 +0530

    added init.el and gitignore
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ init.el
@@ -0,0 +1,23 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit
+		      starter-kit-lisp
+		      starter-kit-bindings
+		      starter-kit-js
+		      starter-kit-ruby
+		      starter-kit-eshell
+                      coffee-mode
+                      haml-mode
+                      scss-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
